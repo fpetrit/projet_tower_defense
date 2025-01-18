ffff5453bd0046b77176c5cd2be4d150678c6bfa
fpetrit <frederic-henri.petrit@dauphine.eu>
2025-01-18 19:41:32 +0100

    ça sera le fichier pour toutes les fonctions qui seront au niveau tour pour update les objets

---
+++ game/round.h
@@ -0,0 +1,16 @@
+#ifndef ROUND_H
+#define ROUND_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+enum damage_type {
+    basic,
+    zone,
+};
+
+enum movement_type {
+    basic,
+};
+
+#endif ROUND_H
