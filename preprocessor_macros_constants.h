b38df20f9ed54d4c1e52776958e0caf2dfeacfd0
fpetrit <frederic-henri.petrit@dauphine.eu>
2025-01-21 18:29:35 +0100

    rajouté header guards

--- preprocessor_macros_constants.h
+++ preprocessor_macros_constants.h
@@ -1,3 +1,9 @@
+#ifndef PREPROCESSOR_MACROS_CONSTANTS_H
+#define PREPROCESSOR_MACROS_CONSTANTS_H
+
 #define CLEAR printf("\033[0;0H"); printf("\033[2J"); // on se place en haut et on efface tout
 #define ROWS 7
 #define COLUMNS 15
+
+
+#endif
