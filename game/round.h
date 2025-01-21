8e1463bab0ffa8b13b6a972cdaf1b2fffa1293e9
fpetrit <frederic-henri.petrit@dauphine.eu>
2025-01-21 18:25:40 +0100

    fonctions de gestion de tour pour les dommages

--- game/round.h
+++ game/round.h
@@ -4,6 +4,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "game.h"
+
 enum damage_type {
     basic,
     zone,
@@ -12,5 +14,21 @@ enum damage_type {
 enum movement_type {
     basic,
 };
+
+/**
+ * @brief Applies the damages that a Tourelle can inflict to Etudiant s during a round.
+ * 
+ * The behaviour of the function (the zone in which Etudiant s receive damages, how much, etc...)
+ * is personalized thanks to the Tourelle.type.
+ */
+void inflict_damage(Tourelle * t);
+
+/**
+ * @brief Applies the damages that an Etudiant can inflict to Tourelle s during a round.
+ * 
+ * The behaviour of the function (the zone in which Tourelle s receive damages, how much, etc...)
+ * is personalized thanks to the Etudiant.type.
+ */
+void inflict_damage(Etudiant * e);
 
 #endif ROUND_H
