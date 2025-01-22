b43dfc1efe0264b1683d3515d2f61a979081cd7f
fpetrit <frederic-henri.petrit@dauphine.eu>
2025-01-22 18:22:44 +0100

    rajouté un nombre max de charactère dans les fichiers input en define

--- preprocessor_macros_constants.h
+++ preprocessor_macros_constants.h
@@ -2,6 +2,7 @@
 #define PREPROCESSOR_MACROS_CONSTANTS_H
 
 #define CLEAR printf("\033[0;0H"); printf("\033[2J"); // on se place en haut et on efface tout
+#define LINE_MAX_CHAR_NO 300
 #define ROWS 7
 #define COLUMNS 15
 
