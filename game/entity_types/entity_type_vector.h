867036d9c02db26be3d080efcd4becef37e313ff
fpetrit <frederic-henri.petrit@dauphine.eu>
2025-01-24 18:14:54 +0100

    déplacé la déclaration dans le .c car .h évidemment cela mène a des multiple definition ld errors

--- game/entity_types/entity_type_vector.h
+++ game/entity_types/entity_type_vector.h
@@ -25,36 +25,8 @@ inline Tagged_entity_type * entity_type_get_type_by_id(Entity_type_vector * vect
     return &vect->arr[id];
 }
 
-Tagged_entity_type * entity_type_get_type_by_abbr(Entity_type_vector * vect, char abbr, Entity_type_tag tag){
+Tagged_entity_type * entity_type_get_type_by_abbr(Entity_type_vector * vect, char abbr, Entity_type_tag tag);
 
-    Tagged_entity_type * res = NULL;
-    bool found = false;
-
-    int i = 0;
-    while ( i < vect->count && ! found ){
-
-        switch (tag)
-        {
-        case ETUDIANT:
-            found = vect->arr[i].type.e_type.abbr == abbr;
-            break;
-
-        case TOURELLE:
-            found = vect->arr[i].type.t_type.abbr == abbr;
-            break;
-        
-        default:
-            break;
-        }
-
-        i++;
-    }
-
-    if (found)
-        res = &vect->arr[i-1];
-
-    return res;
-}
 
 extern Entity_type_vector tourelle_types;
 extern Entity_type_vector etudiant_types;
